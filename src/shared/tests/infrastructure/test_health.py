86865b0d81af961f56afdc24bbbe85b6b0d05c03
Dairo Mosquera <dairo.mosquera@arus.com.co>
2025-05-08 10:26:59 -0500

    refactor: refactor health endpoint

--- src/shared/tests/infrastructure/test_health.py
+++ src/shared/tests/infrastructure/test_health.py
@@ -2,11 +2,10 @@ from fastapi.testclient import TestClient
 
 from main import app
 
-
 client = TestClient(app)
 
 
-def test_read_main():
+def test_read_health():
     response = client.get('/health')
     assert response.status_code == 200
     assert response.json() == {'detail': 'ok'}
