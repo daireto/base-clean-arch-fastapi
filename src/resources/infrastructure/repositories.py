9109ba2ae41997c8f6179a669536a24c83242101
Dairo Mosquera <dairoandres115@outlook.com>
2025-05-23 22:58:39 -0500

    feat: Implement save method

--- src/resources/infrastructure/repositories.py
+++ src/resources/infrastructure/repositories.py
@@ -1,5 +1,17 @@
+from sqlalchemy import create_engine
+from sqlmodel import Field, SQLModel, Session
+
 from resources.domain.models import Resource
 from resources.domain.repositories import ResourcesRepository
+
+
+class ResourceModel(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    url: str
+
+
+engine = create_engine('sqlite:///database.db')
+SQLModel.metadata.create_all(engine)
 
 
 class SQLModelResourcesRepository(ResourcesRepository):
@@ -7,4 +19,7 @@ class SQLModelResourcesRepository(ResourcesRepository):
         pass
 
     def save(self, resource: Resource) -> None:
-        pass
+        resource_model = ResourceModel(url=resource.url)
+        with Session(engine) as session:
+            session.add(resource_model)
+            session.commit()
