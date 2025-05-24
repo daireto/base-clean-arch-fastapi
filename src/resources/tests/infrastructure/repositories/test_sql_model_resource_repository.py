4c3f03ef59b7f20af386aa78eb5b3c2a6d252ba0
Dairo Mosquera <dairoandres115@outlook.com>
2025-05-23 22:58:57 -0500

    test: Green test for saving resource with sql model repository

--- src/resources/tests/infrastructure/repositories/test_sql_model_resource_repository.py
+++ src/resources/tests/infrastructure/repositories/test_sql_model_resource_repository.py
@@ -1,14 +1,26 @@
-from sqlmodel import Session
+import pytest
+from sqlmodel import SQLModel, Session, select
 from resources.domain.models import Resource
 from resources.domain.value_objects import ResourceUrl
-from resources.infrastructure.repositories import SQLModelResourcesRepository
+from resources.infrastructure.repositories import (
+    ResourceModel,
+    SQLModelResourcesRepository,
+    engine,
+)
 
 
 class TestSQLModelResourcesRepository:
+    @pytest.fixture(autouse=True)
+    def cleanup(self):
+        yield
+        SQLModel.metadata.drop_all(engine)
+
     def test_save_resource_to_database(self) -> None:
         repo = SQLModelResourcesRepository()
+
         repo.save(Resource(ResourceUrl(value='https://example.com')))
+
         with Session(engine) as session:
-            stmt = select(Resource)
+            stmt = select(ResourceModel)
             resource = session.exec(stmt).first()
             assert resource.url == 'https://example.com'
