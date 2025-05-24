f6a28dc2421b5061fd9231084479c445f1ad1523
Dairo Mosquera <dairoandres115@outlook.com>
2025-05-23 23:22:32 -0500

    feat: Implement method to fetch all resources

--- src/resources/infrastructure/repositories.py
+++ src/resources/infrastructure/repositories.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
-from sqlmodel import Field, SQLModel, Session
+from sqlmodel import Field, Session, SQLModel, select
 
 from resources.domain.models import Resource
 from resources.domain.repositories import ResourcesRepository
+from resources.domain.value_objects import ResourceUrl
 
 
 class ResourceModel(SQLModel, table=True):
@@ -16,10 +17,13 @@ SQLModel.metadata.create_all(engine)
 
 class SQLModelResourcesRepository(ResourcesRepository):
     def all(self) -> list[Resource]:
-        pass
+        with Session(engine) as session:
+            resources = session.exec(select(ResourceModel)).all()
+
+        return [Resource(ResourceUrl(value=resource.url)) for resource in resources]
 
     def save(self, resource: Resource) -> None:
-        resource_model = ResourceModel(url=resource.url)
+        resource_model = ResourceModel(url=resource.url.value)
         with Session(engine) as session:
             session.add(resource_model)
             session.commit()
