2a26d56acb5bba011f3184deaa603970c3c2a1b9
Dairo Mosquera <dairoandres115@outlook.com>
2025-06-30 23:14:53 -0500

    feat: Add resource type value object

--- src/resources/domain/value_objects.py
+++ src/resources/domain/value_objects.py
@@ -2,23 +2,23 @@ from dataclasses import dataclass
 
 import validators
 
-from resources.domain.exceptions import InvalidURLError
+from resources.domain.errors import InvalidURLError, ResourceTypeNotSupportedError
+from shared.domain.value_object import ValueObject
 
 
 @dataclass(frozen=True, kw_only=True)
-class ResourceUrl:
+class ResourceType(ValueObject):
     value: str
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
+        if self.value not in ['image', 'video', 'audio', 'text', 'other']:
+            raise ResourceTypeNotSupportedError(self.value)
+
+
+@dataclass(frozen=True, kw_only=True)
+class ResourceUrl(ValueObject):
+    value: str
+
+    def __post_init__(self) -> None:
         if not validators.url(self.value):
             raise InvalidURLError(self.value)
-
-    def __str__(self) -> str:
-        return self.value
-
-    def __eq__(self, value: 'ResourceUrl | str') -> bool:
-        return (
-            self.value == value.value
-            if isinstance(value, ResourceUrl)
-            else self.value == value
-        )
