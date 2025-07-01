f7402a8ef9a137d0eb91fee9c641fc5e453a5067
Dairo Mosquera <dairoandres115@outlook.com>
2025-06-30 23:16:17 -0500

    feat: Add module for DB models

---
+++ src/resources/infrastructure/models.py
@@ -0,0 +1,15 @@
+from uuid import UUID
+
+from sqlactive import ActiveRecordBaseModel
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class DBModel(ActiveRecordBaseModel):
+    __abstract__ = True
+    id: Mapped[UUID] = mapped_column(primary_key=True)
+
+
+class SQLiteResourceModel(DBModel):
+    name: Mapped[str] = mapped_column()
+    url: Mapped[str] = mapped_column()
+    type: Mapped[str] = mapped_column()
