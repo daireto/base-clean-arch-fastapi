3ebdaa94721885770d11683105135cde20e0dd73
Dairo Mosquera <dairoandres115@outlook.com>
2025-06-30 23:42:54 -0500

    feat: Remove id from base model

--- src/resources/infrastructure/models.py
+++ src/resources/infrastructure/models.py
@@ -6,12 +6,12 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 class DBModel(ActiveRecordBaseModel):
     __abstract__ = True
-    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
 
 
 class SQLiteResourceModel(DBModel):
     __tablename__ = 'resources'
 
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
     name: Mapped[str] = mapped_column()
     url: Mapped[str] = mapped_column()
     type: Mapped[str] = mapped_column()
