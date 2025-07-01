a68c19e80aba04f74195943d4eaf9445ff98b861
Dairo Mosquera <dairoandres115@outlook.com>
2025-06-30 23:21:02 -0500

    feat: Add DB connection and models initialization logic and include routers

--- src/main.py
+++ src/main.py
@@ -1,6 +1,24 @@
+from collections.abc import AsyncIterator
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+from sqlactive import DBConnection
 
-from shared.infrastructure.api import router as shared_router
+from resources.api import router as resources_router
+from resources.infrastructure.models import DBModel as ResourcesDBModel
+from shared.api import router as shared_router
+from shared.config import SharedSettings
 
-app = FastAPI()
+conn = DBConnection(SharedSettings.DATABASE_URL, echo=False)
+
+
+@asynccontextmanager
+async def lifespan(_: FastAPI) -> AsyncIterator[None]:
+    await conn.init_db(ResourcesDBModel)
+    yield
+
+
+app = FastAPI(default_response_class=ORJSONResponse, lifespan=lifespan)
 app.include_router(shared_router)
+app.include_router(resources_router)
