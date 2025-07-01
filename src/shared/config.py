f4c70d95c8cc1bf1d247fc59e343f8cf7aaf08b8
Dairo Mosquera <dairoandres115@outlook.com>
2025-06-30 23:35:20 -0500

    fix: Set correct plugin

--- src/shared/config.py
+++ src/shared/config.py
@@ -1,11 +1,11 @@
 from starlette.config import Config
 
-config = Config('.env')
+config = Config()
 
 
 class SharedSettings:
     DATABASE_URL = config(
         'DATABASE_URL',
         cast=str,
-        default='aiosqlite:///database.db',
+        default='sqlite+aiosqlite:///database.db',
     )
