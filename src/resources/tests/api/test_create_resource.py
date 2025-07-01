4951bdfe060c51e099b4845dce840bedb8f4db8f
Dairo Mosquera <dairoandres115@outlook.com>
2025-07-01 00:00:55 -0500

    test: Green test for saving resource with sqlite repo

---
+++ src/resources/tests/api/test_create_resource.py
@@ -0,0 +1,21 @@
+from fastapi.testclient import TestClient
+
+from main import app
+
+
+def test_create_resource_and_return_200():
+    data = {
+        'name': 'Random Image',
+        'url': 'https://picsum.photos/200/200',
+        'type': 'image',
+    }
+
+    with TestClient(app) as client:
+        response = client.post('/resources/', json=data)
+
+    assert response.status_code == 200
+
+    response_content = response.json()
+    assert response_content['name'] == data['name']
+    assert response_content['url'] == data['url']
+    assert response_content['type'] == data['type']
