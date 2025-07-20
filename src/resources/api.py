0e17f21bef357ab431db10e59e47114650986dfd
Dairo Mosquera <dairoandres115@outlook.com>
2025-07-20 13:27:48 -0500

    feat: Add endpoint to update resources

--- src/resources/api.py
+++ src/resources/api.py
@@ -4,6 +4,10 @@ from src.resources.application.create_resource import (
     CreateResourceCommand,
     CreateResourceHandler,
 )
+from src.resources.application.update_resource import (
+    UpdateResourceCommand,
+    UpdateResourceHandler,
+)
 from src.resources.di import deps
 from src.resources.domain.repositories import ResourceRepositoryABC
 from src.resources.dtos import CreateResourceRequestDTO, ResourceResponseDTO
@@ -18,6 +22,23 @@ async def create_resource(
 ) -> ResourceResponseDTO:
     resource = await CreateResourceHandler(repo).handle(
         CreateResourceCommand(
+            name=dto.name,
+            url=dto.url,
+            type=dto.type,
+        )
+    )
+    return ResourceResponseDTO.from_domain(resource)
+
+
+@router.put('/resources/{id_}')
+async def update_resource(
+    id_: str,
+    dto: CreateResourceRequestDTO,
+    repo: ResourceRepositoryABC = deps.depends(ResourceRepositoryABC),
+) -> ResourceResponseDTO:
+    resource = await UpdateResourceHandler(repo).handle(
+        UpdateResourceCommand(
+            id_=id_,
             name=dto.name,
             url=dto.url,
             type=dto.type,
