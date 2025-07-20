05a6a5e43e57c8dbbf0e3ab08afae1337d658a4a
Dairo Mosquera <dairoandres115@outlook.com>
2025-07-20 14:19:23 -0500

    test: Rename folder to api

--- src/shared/tests/api/test_health.py
+++ src/shared/tests/api/test_health.py
@@ -3,10 +3,10 @@ from fastapi.testclient import TestClient
 
 from src.main import app
 
-client = TestClient(app)
-
 
 def test_read_health():
-    response = client.get('/health')
+    with TestClient(app) as client:
+        response = client.get('/health')
+
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {'detail': 'ok'}
