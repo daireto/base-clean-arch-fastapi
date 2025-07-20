2f3722164352bb49ad03a91acab5a113e750580a
Dairo Mosquera <dairoandres115@outlook.com>
2025-07-20 13:14:52 -0500

    feat: Move hardcoded value to a constant

--- src/shared/domain/utils.py
+++ src/shared/domain/utils.py
@@ -1,5 +1,7 @@
 from uuid import UUID
 
+EMPTY_UUID = '00000000-0000-0000-0000-000000000000'
+
 
 def empty_uuid() -> UUID:
-    return UUID('00000000-0000-0000-0000-000000000000')
+    return UUID(EMPTY_UUID)
