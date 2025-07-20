02c6f5c5597e9f62ffde6d66673e077a8f1f08db
Dairo Mosquera <dairoandres115@outlook.com>
2025-07-20 14:15:17 -0500

    test: Green test for list resources endpoint

--- src/resources/tests/api/test_list_resources.py
+++ src/resources/tests/api/test_list_resources.py
@@ -2,17 +2,26 @@ import pytest
 import pytest_asyncio
 from fastapi import status
 from fastapi.testclient import TestClient
+from odata_v4_query import ODataQueryOptions
 
 from src.main import app
 from src.resources.di import container
 from src.resources.domain.entities import Resource
 from src.resources.domain.repositories import ResourceRepositoryABC
 from src.resources.domain.value_objects import ResourceType, ResourceUrl
+from src.shared import settings
 
 
 @pytest_asyncio.fixture()
 async def resources():
     repo = container[ResourceRepositoryABC]
+
+    current_resources = await repo.all(
+        odata_options=ODataQueryOptions(top=settings.MAX_RECORDS_PER_PAGE),
+    )
+    for resource in current_resources:
+        await repo.delete(resource.id)
+
     return [
         await repo.create(
             Resource(
@@ -31,7 +40,7 @@ async def resources():
     ]
 
 
-@pytest.mark.asyncio
+@pytest.mark.asyncio(loop_scope='module')
 class TestListResources:
     async def test_list_resources_and_return_200(self, resources: list[Resource]):
         with TestClient(app) as client:
@@ -40,12 +49,9 @@ class TestListResources:
         assert response.status_code == status.HTTP_200_OK
 
         response_content = response.json()
-        expected_resources_count = 2
-        assert len(response_content) == expected_resources_count
-        assert response_content[0]['id'] == str(resources[0].id)
-        assert response_content[1]['id'] == str(resources[1].id)
+        assert len(response_content) == len(resources)
 
-    async def test_limit_number_of_resources_returned(self, resources: list[Resource]):
+    async def test_limit_number_of_resources_returned(self):
         with TestClient(app) as client:
             response = client.get('/resources/?$top=1')
 
@@ -54,4 +60,3 @@ class TestListResources:
         response_content = response.json()
         expected_resources_count = 1
         assert len(response_content) == expected_resources_count
-        assert response_content[0]['id'] == str(resources[0].id)
