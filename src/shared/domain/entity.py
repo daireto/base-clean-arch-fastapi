332b92f5777c4b1f3c09ade55ab2746472b57a9b
Dairo Mosquera <dairoandres115@outlook.com>
2025-10-19 14:06:06 -0500

    feat: Make Entity class abstract and update import path for empty_uuid
    Updated the Entity class to inherit from ABC, making it an abstract class. Also modified the import statement for empty_uuid to reflect its new location.

--- src/shared/domain/entity.py
+++ src/shared/domain/entity.py
@@ -1,12 +1,13 @@
 import time
+from abc import ABC
 from uuid import UUID
 
 from pydantic import BaseModel, Field
 
-from src.shared.domain.utils import empty_uuid
+from src.shared.utils import empty_uuid
 
 
-class Entity(BaseModel):
+class Entity(BaseModel, ABC):
     id: UUID = Field(default_factory=empty_uuid)
     created_at: float = Field(default_factory=time.time)
     updated_at: float = Field(default_factory=time.time)
