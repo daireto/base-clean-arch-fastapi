d3d403e6562a41e15b2a17790ad198b978d602a1
Dairo Mosquera <dairoandres115@outlook.com>
2025-10-19 14:06:50 -0500

    feat: Make ValueObject generic and update value type
    Refactor the ValueObject class to be generic, allowing for type-safe value handling. Updated the constructor and related methods to accommodate the new generic type.

--- src/shared/domain/value_object.py
+++ src/shared/domain/value_object.py
@@ -1,17 +1,28 @@
-from pydantic import BaseModel, ConfigDict
+from typing import Generic, TypeVar
+
+from pydantic import BaseModel, ConfigDict, model_serializer
+
+T = TypeVar('T')
 
 
-class ValueObject(BaseModel):
+class ValueObject(BaseModel, Generic[T]):
     model_config = ConfigDict(frozen=True)
 
-    value: object
+    value: T
 
-    def __init__(self, value: object) -> None:
+    def __init__(self, value: T) -> None:
         super().__init__(value=value)
         self.validate()
 
+    def get_value(self) -> T:
+        return self.value
+
     def validate(self) -> None:
         pass
+
+    @model_serializer(mode='plain')
+    def serialize_model(self) -> T:
+        return self.get_value()
 
     def __str__(self) -> str:
         return str(self.value)
