df5a5b5817f89154d19dcbe654c3da282b960437
Dairo Mosquera <dairoandres115@outlook.com>
2025-10-19 14:02:24 -0500

    feat: Remove resource management functionality to move all the related files to a "features" folder

--- src/resources/di.py
+++
@@ -1,9 +0,0 @@
-from lagom import Container
-from lagom.integrations.fast_api import FastApiIntegration
-
-from src.resources.domain.repositories import ResourceRepositoryABC
-from src.resources.infrastructure.repositories.sqlite import SQLiteResourceRepository
-
-container = Container()
-container[ResourceRepositoryABC] = SQLiteResourceRepository()
-deps = FastApiIntegration(container)
