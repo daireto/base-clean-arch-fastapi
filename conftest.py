2908c2e0f1473d3c523ef78aad4eb66e5044f335
Dairo Mosquera <dairoandres115@outlook.com>
2025-10-20 22:33:13 -0500

    feat: Add new test fixtures for resource repository
    Introduces new pytest fixtures for testing resource creation using a mock repository. This includes a synchronous fixture for the repository and asynchronous fixtures for individual and multiple resources.

---
+++ conftest.py
@@ -0,0 +1,50 @@
+from collections.abc import Generator
+
+import pytest
+import pytest_asyncio
+
+from src.features.resources.di import deps
+from src.features.resources.domain.entities import Resource
+from src.features.resources.domain.interfaces.repositories import ResourceRepositoryABC
+from src.features.resources.infrastructure.persistence.repositories.mock import (
+    MockResourceRepository,
+)
+
+
+@pytest.fixture
+def repo() -> Generator[ResourceRepositoryABC]:
+    with deps.override_for_test() as test_container:
+        repo = MockResourceRepository()
+        test_container[ResourceRepositoryABC] = repo
+        yield repo
+
+
+@pytest_asyncio.fixture
+async def resource(repo: ResourceRepositoryABC) -> Resource:
+    return await repo.create(
+        Resource.Builder()
+        .with_name('Random Image')
+        .with_url('https://example.com')
+        .with_type('image')
+        .build()
+    )
+
+
+@pytest_asyncio.fixture
+async def resources(repo: ResourceRepositoryABC) -> list[Resource]:
+    return [
+        await repo.create(
+            Resource.Builder()
+            .with_name('Random Image')
+            .with_url('https://example.com')
+            .with_type('image')
+            .build()
+        ),
+        await repo.create(
+            Resource.Builder()
+            .with_name('Random Image')
+            .with_url('https://example.org')
+            .with_type('image')
+            .build()
+        ),
+    ]
