d11295d224466463a21d21a84e54af928604841b
Dairo Mosquera <dairoandres115@outlook.com>
2025-10-28 17:44:51 -0500

    refactor: Refactor health check to use Pydantic and enum
    Refactor ServerHealth class to use Pydantic for responses and add HealthStatus enum.

--- src/core/health.py
+++ src/core/health.py
@@ -1,10 +1,22 @@
+from enum import Enum
 from functools import lru_cache
 
+from pydantic import BaseModel
 
-class ServerHealth:
+
+class HealthStatus(str, Enum):
+    HEALTHY = 'healthy'
+    UNHEALTHY = 'unhealthy'
+
+
+class ServerHealthResponse(BaseModel):
+    message: str
+    status: HealthStatus
+
+
+class _ServerHealth:
     def __init__(self) -> None:
-        self._is_healthy = True
-        self._unhealthy_reason = ''
+        self.reset()
 
     @property
     def is_healthy(self) -> bool:
@@ -25,19 +37,28 @@ class ServerHealth:
     def get_unhealthy_reason(self) -> str:
         return self._unhealthy_reason
 
-    def to_response(self) -> dict[str, str]:
-        return {
-            'message': 'pong' if self.is_healthy else self.get_unhealthy_reason(),
-            'status': 'ok' if self.is_healthy else 'error',
-        }
+    def to_response(self) -> ServerHealthResponse:
+        return ServerHealthResponse(
+            message='ok' if self.is_healthy else self.get_unhealthy_reason(),
+            status=HealthStatus.HEALTHY
+            if self.is_healthy
+            else HealthStatus.UNHEALTHY,
+        )
+
+    def __str__(self) -> str:
+        return (
+            HealthStatus.HEALTHY
+            if self.is_healthy
+            else f'{HealthStatus.UNHEALTHY}: {self.get_unhealthy_reason()}'
+        )
 
     def __bool__(self) -> bool:
         return self.is_healthy
 
 
 @lru_cache
-def get_server_health() -> ServerHealth:
-    return ServerHealth()
+def get_server_health() -> _ServerHealth:
+    return _ServerHealth()
 
 
 server_health = get_server_health()
