07a1d3a136f1e4e396187e80793f5877df2c8ef3
Dairo Mosquera <dairoandres115@outlook.com>
2025-11-28 22:00:15 -0500

    refactor: Enhance ResourceType validation to include supported types in error message
    Updated the ResourceType validation to provide a list of supported types in the ResourceTypeNotSupportedError. This improves error clarity and helps users understand valid input options.

--- src/modules/resources/domain/value_objects.py
+++ src/modules/resources/domain/value_objects.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlparse
+
 import validators
 
 from modules.resources.domain.exceptions import ResourceTypeNotSupportedError
@@ -7,11 +9,24 @@ from shared.domain.value_object import ValueObject
 
 class ResourceType(ValueObject[str]):
     def validate(self) -> None:
-        if self.value not in ['image', 'video', 'audio', 'text', 'other']:
-            raise ResourceTypeNotSupportedError(self.value)
+        supported_types = ['image', 'video', 'audio', 'text', 'other']
+        if self.value not in supported_types:
+            raise ResourceTypeNotSupportedError(self.value, supported_types)
 
 
 class ResourceUrl(ValueObject[str]):
     def validate(self) -> None:
         if not validators.url(self.value):
             raise InvalidURLError(self.value)
+
+    def get_scheme(self) -> str:
+        return self.value.split('://')[0]
+
+    def get_domain(self) -> str:
+        return urlparse(self.value).netloc
+
+    def domain_equals_to(self, domain: str) -> bool:
+        return domain.lower() in self.get_domain().lower()
+
+    def scheme_equals_to(self, scheme: str) -> bool:
+        return scheme.lower() == self.get_scheme().lower()
