eb30d5b1a41f463f762903d2795bedc5d8b5a390
Dairo Mosquera <dairoandres115@outlook.com>
2026-03-23 01:05:01 -0500

    refactor: Replace lagom container with dishka provider for dependency injection
    Updated the dependency injection mechanism by replacing the lagom container with the dishka provider, enhancing the structure and maintainability of the code.

--- src/modules/resources/di.py
+++ src/modules/resources/di.py
@@ -1,16 +1,9 @@
-from lagom import Container
-from lagom.integrations.fast_api import FastApiIntegration
+from dishka import Provider, Scope
 
 from modules.resources.domain.interfaces.repositories import ResourceRepositoryABC
 from modules.resources.infrastructure.persistence.repositories.sqlite import (
     SQLiteResourceRepository,
 )
 
-
-def configure_dependencies(container: Container) -> None:
-    container[ResourceRepositoryABC] = SQLiteResourceRepository()
-
-
-container = Container()
-configure_dependencies(container)
-deps = FastApiIntegration(container)
+provider = Provider(scope=Scope.APP)
+provider.provide(SQLiteResourceRepository, provides=ResourceRepositoryABC)
