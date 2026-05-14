dd7152478c4ae7baa8114ca9c4f30db13256d580
Dairo Mosquera <dairoandres115@outlook.com>
2026-05-13 20:48:03 -0500

    refactor: Organize imports and enhance resource handler instrumentation
    Refactored import statements for better readability and grouped related imports. Enhanced resource handlers by integrating instrumentation for logging purposes, improving traceability and debugging capabilities.

--- src/modules/resources/presentation/api.py
+++ src/modules/resources/presentation/api.py
@@ -2,7 +2,7 @@ from dishka import FromDishka
 from dishka.integrations.fastapi import DishkaRoute
 from fastapi import APIRouter, Request, Response
 
-from app.config import settings
+from config import settings
 from modules.resources.application.use_cases.create_resource import (
     CreateResourceCommand,
     CreateResourceHandler,
@@ -23,7 +23,24 @@ from modules.resources.application.use_cases.update_resource import (
     UpdateResourceCommand,
     UpdateResourceHandler,
 )
-from modules.resources.domain.interfaces.repositories import ResourceRepositoryABC
+from modules.resources.domain.interfaces.repositories import (
+    ResourceRepositoryABC,
+)
+from modules.resources.infrastructure.instrumentation.use_cases.create_resource import (
+    CreateResourceInstrumentation,
+)
+from modules.resources.infrastructure.instrumentation.use_cases.delete_resource import (
+    DeleteResourceInstrumentation,
+)
+from modules.resources.infrastructure.instrumentation.use_cases.get_resource import (
+    GetResourceInstrumentation,
+)
+from modules.resources.infrastructure.instrumentation.use_cases.list_resources import (
+    ListResourcesInstrumentation,
+)
+from modules.resources.infrastructure.instrumentation.use_cases.update_resource import (
+    UpdateResourceInstrumentation,
+)
 from modules.resources.presentation.dtos import (
     CreateResourceRequestDTO,
     ResourceResponseDTO,
@@ -38,13 +55,20 @@ router = APIRouter(route_class=DishkaRoute)
 
 @router.get('/resources/{id_}')
 async def get_resource(
+    request: Request,
     id_: str,
     repo: FromDishka[ResourceRepositoryABC],
 ) -> ResourceResponseDTO:
     uuid_id = uuid_from_string(id_)
     command = GetResourceCommand(id=uuid_id)
+    handler = GetResourceHandler(
+        resource_repository=repo,
+        instrumentation=GetResourceInstrumentation(
+            logger=request.app.state.get_child_logger('resources.get'),
+        ),
+    )
 
-    if result := await GetResourceHandler(repo).handle(command):
+    if result := await handler.handle(command):
         return ResourceResponseDTO.from_entity(result.unwrap_value())
 
     raise result.unwrap_error()
@@ -55,12 +79,21 @@ async def list_resources(
     request: Request,
     repo: FromDishka[ResourceRepositoryABC],
 ) -> PaginatedResponseDTO[ResourceResponseDTO]:
-    odata = ODataHelper.get_from_query(request.url.query, settings.max_records_per_page)
+    odata = ODataHelper.get_from_query(
+        query_string=request.url.query,
+        max_top=settings.max_records_per_page,
+    )
     command = ListResourcesCommand(
         odata=odata,
     )
+    handler = ListResourcesHandler(
+        resource_repository=repo,
+        instrumentation=ListResourcesInstrumentation(
+            logger=request.app.state.get_child_logger('resources.list'),
+        ),
+    )
 
-    if result := await ListResourcesHandler(repo).handle_with_count(command):
+    if result := await handler.handle_with_count(command):
         resources = result.unwrap_value()
         items = ResourceResponseDTO.from_entities(resources)
         return PaginatedResponseDTO.from_odata_query_result(
@@ -72,12 +105,19 @@ async def list_resources(
 
 @router.post('/resources/')
 async def create_resource(
+    request: Request,
     dto: CreateResourceRequestDTO,
     repo: FromDishka[ResourceRepositoryABC],
 ) -> ResourceResponseDTO:
     command = CreateResourceCommand(name=dto.name, url=dto.url, type=dto.type)
+    handler = CreateResourceHandler(
+        resource_repository=repo,
+        instrumentation=CreateResourceInstrumentation(
+            logger=request.app.state.get_child_logger('resources.create'),
+        ),
+    )
 
-    if result := await CreateResourceHandler(repo).handle(command):
+    if result := await handler.handle(command):
         return ResourceResponseDTO.from_entity(result.unwrap_value())
 
     raise result.unwrap_error()
@@ -85,6 +125,7 @@ async def create_resource(
 
 @router.put('/resources/{id_}')
 async def update_resource(
+    request: Request,
     id_: str,
     dto: CreateResourceRequestDTO,
     repo: FromDishka[ResourceRepositoryABC],
@@ -96,8 +137,14 @@ async def update_resource(
         url=dto.url,
         type=dto.type,
     )
+    handler = UpdateResourceHandler(
+        resource_repository=repo,
+        instrumentation=UpdateResourceInstrumentation(
+            logger=request.app.state.get_child_logger('resources.update'),
+        ),
+    )
 
-    if result := await UpdateResourceHandler(repo).handle(command):
+    if result := await handler.handle(command):
         return ResourceResponseDTO.from_entity(result.unwrap_value())
 
     raise result.unwrap_error()
@@ -105,12 +152,19 @@ async def update_resource(
 
 @router.delete('/resources/{id_}')
 async def delete_resource(
+    request: Request,
     id_: str,
     repo: FromDishka[ResourceRepositoryABC],
 ) -> Response:
     command = DeleteResourceCommand(id=uuid_from_string(id_))
+    handler = DeleteResourceHandler(
+        resource_repository=repo,
+        instrumentation=DeleteResourceInstrumentation(
+            logger=request.app.state.get_child_logger('resources.delete'),
+        ),
+    )
 
-    if result := await DeleteResourceHandler(repo).handle(command):
+    if result := await handler.handle(command):
         return NoContent()
 
     raise result.unwrap_error()
