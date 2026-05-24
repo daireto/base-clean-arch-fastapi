39ccf16ec1044de3b4897dfd96c2b4f7ac0eb285
Dairo Mosquera <dairoandres115@outlook.com>
2026-05-24 02:26:14 -0500

    feat: Enhance update method in Entity class
    Refactored the update method to handle both BaseModel and dictionary inputs, ensuring proper handling of ValueObject attributes and updating the timestamp correctly.

--- src/shared/domain/bases/entity.py
+++ src/shared/domain/bases/entity.py
@@ -1,8 +1,11 @@
 import time
 from abc import ABC, abstractmethod
+from typing import Any
 from uuid import UUID, uuid4
 
 from pydantic import BaseModel, Field
+
+from shared.domain.bases.value_object import ValueObject
 
 
 class Entity(BaseModel, ABC):
@@ -10,8 +13,26 @@ class Entity(BaseModel, ABC):
     created_at: float = Field(default_factory=time.time)
     updated_at: float = Field(default_factory=time.time)
 
-    def set_id(self, id_: UUID) -> None:
-        self.id = id_
+    def update(self, data: BaseModel | dict[str, Any]):  # noqa: ANN201
+        if isinstance(data, BaseModel):
+            dumped_data = data.model_dump(exclude_unset=True)
+        else:
+            dumped_data = data
+
+        dumped_data['updated_at'] = time.time()
+        updated_data = {**self.model_dump(), **dumped_data}
+        new_model = {}
+
+        for key, value in updated_data.items():
+            if not hasattr(self, key):
+                continue
+            attr = getattr(self, key)
+            if isinstance(attr, ValueObject):
+                new_model[key] = attr.__class__(value=value)
+            else:
+                new_model[key] = value
+
+        return self.model_validate(new_model)
 
     class Builder(ABC):
         def __init__(self) -> None:
