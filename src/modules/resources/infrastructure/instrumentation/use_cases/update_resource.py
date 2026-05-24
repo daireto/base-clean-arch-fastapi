4a573f47a8da58aade884d6b04d79fbe57bc8633
Dairo Mosquera <dairoandres115@outlook.com>
2026-05-24 02:28:40 -0500

    feat: Enhance UpdateResourceInstrumentation with creation handling
    Updated the after method to differentiate between resource creation and update. Added not_found method for handling resource not found scenarios.

--- src/modules/resources/infrastructure/instrumentation/use_cases/update_resource.py
+++ src/modules/resources/infrastructure/instrumentation/use_cases/update_resource.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from modules.resources.domain.entities import Resource
 from shared.application.instrumentation import Instrumentation
 
@@ -6,8 +8,17 @@ class UpdateResourceInstrumentation(Instrumentation):
     def before(self, resource: Resource) -> None:
         super().before('Updating resource', resource_id=resource.id)
 
-    def after(self, resource: Resource) -> None:
-        super().after('Resource updated', resource_id=resource.id)
+    def after(self, resource: Resource, created: bool = False) -> None:
+        if created:
+            super().after(
+                'Resource created as it did not exist',
+                resource_id=resource.id,
+            )
+        else:
+            super().after('Resource updated', resource_id=resource.id)
 
     def error(self, error: Exception) -> None:
         super().error('Error updating resource', error)
+
+    def not_found(self, id_: UUID) -> None:
+        super().not_found('Resource not found', resource_id=id_)
