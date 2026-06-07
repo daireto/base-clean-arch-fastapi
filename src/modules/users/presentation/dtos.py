6ea90f0e8aefca96f8291a534be1d8679828970f
Dairo Mosquera <dairoandres115@outlook.com>
2026-06-06 22:13:14 -0500

    feat(users): update password field to use HashedSecretStr
    Replaced SecretStr with HashedSecretStr in CreateUserRequestDTO, UpdateUserRequestDTO, and PartialUpdateUserRequestDTO classes. This change enhances password security by ensuring that passwords are hashed before storage. Updated the relevant fields to reflect this change while maintaining existing validation constraints.

--- src/modules/users/presentation/dtos.py
+++ src/modules/users/presentation/dtos.py
@@ -1,8 +1,9 @@
-from pydantic import EmailStr, Field, SecretStr
+from pydantic import EmailStr, Field
 
 from modules.users.domain.entities import User
 from modules.users.domain.enums import Gender
 from shared.presentation.dtos import EntityResponseDTO, RequestDTO, ResponseDTO
+from shared.utils.validation_types import HashedSecretStr
 
 
 class CreateUserRequestDTO(RequestDTO):
@@ -10,7 +11,7 @@ class CreateUserRequestDTO(RequestDTO):
     fullname: str = Field(..., description='Full name of the user')
     email: EmailStr = Field(..., description='Email address of the user')
     gender: Gender = Field(..., description='Gender of the user')
-    password: SecretStr = Field(
+    password: HashedSecretStr = Field(
         ..., min_length=8, max_length=80, description='Password of the user'
     )
 
@@ -20,7 +21,7 @@ class UpdateUserRequestDTO(RequestDTO):
     fullname: str = Field(..., description='Full name of the user')
     email: EmailStr = Field(..., description='Email address of the user')
     gender: Gender = Field(..., description='Gender of the user')
-    password: SecretStr | None = Field(
+    password: HashedSecretStr | None = Field(
         None, min_length=8, max_length=80, description='Password of the user'
     )
 
@@ -30,6 +31,9 @@ class PartialUpdateUserRequestDTO(RequestDTO):
     fullname: str | None = Field(None, description='Full name of the user')
     email: EmailStr | None = Field(None, description='Email address of the user')
     gender: Gender | None = Field(None, description='Gender of the user')
+    password: HashedSecretStr | None = Field(
+        None, min_length=8, max_length=80, description='Password of the user'
+    )
 
 
 class UserDTO(EntityResponseDTO):
