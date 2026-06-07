7698b3e3b58050807fbd0f38aa63d4d8bfb186b4
Dairo Mosquera <dairoandres115@outlook.com>
2026-06-06 20:03:26 -0500

    feat(shared): implement use case instrumentation classes
    Introduced a new set of use case instrumentation classes to enhance logging and monitoring capabilities across various use cases. This includes the base class `UseCaseInstrumentation` and specific implementations for creation, deletion, retrieval, listing, and updating use cases. Each class provides hooks for before and after actions, as well as error handling and not found scenarios, allowing for better tracking of application behavior and easier debugging.

---
+++ src/shared/application/instrumentation.py
@@ -0,0 +1,74 @@
+from uuid import UUID
+
+from structlog.stdlib import BoundLogger
+
+from shared.domain.bases.entity import Entity
+
+
+class UseCaseInstrumentation:
+    _logger: BoundLogger
+
+    def __init__(self, logger: BoundLogger | None = None) -> None:
+        if logger:
+            self._logger = logger
+
+    def set_logger(self, logger: BoundLogger) -> None:
+        self._logger = logger
+
+    def before(self, *args, **kwargs) -> None:
+        pass
+
+    def after(self, *args, **kwargs) -> None:
+        pass
+
+    def error(self, error: Exception) -> None:
+        pass
+
+    def not_found(self, *args, **kwargs) -> None:
+        pass
+
+
+class CreationUseCaseInstrumentation[T: Entity](UseCaseInstrumentation):
+    def before(self, entity: T) -> None:
+        pass
+
+    def after(self, entity: T) -> None:
+        pass
+
+
+class DeletionUseCaseInstrumentation(UseCaseInstrumentation):
+    def before(self, id_: UUID) -> None:
+        pass
+
+    def after(self, id_: UUID) -> None:
+        pass
+
+
+class RetrievalUseCaseInstrumentation[T: Entity](UseCaseInstrumentation):
+    def before(self, id_: UUID) -> None:
+        pass
+
+    def after(self, entity: T) -> None:
+        pass
+
+    def not_found(self, id_: UUID) -> None:
+        pass
+
+
+class ListingUseCaseInstrumentation[T: Entity](UseCaseInstrumentation):
+    def before(self) -> None:
+        pass
+
+    def after(self, entities: list[T]) -> None:
+        pass
+
+
+class UpdateUseCaseInstrumentation[T: Entity](UseCaseInstrumentation):
+    def before(self, entity: T) -> None:
+        pass
+
+    def after(self, entity: T, created: bool = False) -> None:
+        pass
+
+    def not_found(self, id_: UUID) -> None:
+        pass
